8f51208a640e1087ac94c8841281119facf589cf
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2023-09-03 10:45:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,4 @@ navToggle.addEventListener('click', () => {
 });
 
 
-    document.getElementById('downloadButton').addEventListener('click', function() {
-        // Define the filename for the downloaded file
-        var filename = 'resume.pdf';
-
-        // Create a blob object for the resume file
-        var blob = new Blob([filename], { type: 'application/pdf' });
-
-        // Create a temporary URL for the blob object
-        var url = URL.createObjectURL(blob);
-
-        // Create a link element to trigger the download
-        var link = document.createElement('a');
-        link.href = url;
-        link.download = filename;
-
-        // Click the link to initiate the download
-        link.click();
-    });
-
+   
